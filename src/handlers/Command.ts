10e60eee312386d6b6afa051b5391134cb342eb3
dady_ocat@hotmail.com <dady_ocat@hotmail.com>
2023-10-29 18:02:43 +0700

    update .gitignore and fix check dir

--- src/handlers/Command.ts
+++ src/handlers/Command.ts
@@ -1,23 +1,35 @@
 import { Client, Routes, SlashCommandBuilder } from "discord.js";
 import { REST } from "@discordjs/rest"
-import { readdirSync,readdir } from "fs";
+import { readdirSync,existsSync } from "fs";
 import { join } from "path";
 import { color } from "../functions";
 import { Command, SlashCommand } from "../types";
+
 
 module.exports = (client : Client) => {
     const slashCommands : SlashCommandBuilder[] = []
     const commands : Command[] = []
 
     let slashCommandsDir = join(__dirname,"../slashCommands")
+    let commandsDir = join(__dirname,"../commands")
+    
+    if(existsSync(slashCommandsDir)){
+        readdirSync(slashCommandsDir).forEach(file => {
+            if (!file.endsWith(".js")) return;
+            let command : SlashCommand = require(`${slashCommandsDir}/${file}`).default
+            slashCommands.push(command.command)
+            client.slashCommands.set(command.command.name, command)
+        });
+    }
 
-    readdirSync(slashCommandsDir).forEach(file => {
-        if (!file.endsWith(".js")) return;
-        let command : SlashCommand = require(`${slashCommandsDir}/${file}`).default
-        slashCommands.push(command.command)
-        client.slashCommands.set(command.command.name, command)
-    })
-
+    if(existsSync(commandsDir)){
+        readdirSync(commandsDir).forEach((file:String) => {
+            if (!file.endsWith(".js")) return;
+            let command : Command = require(`${commandsDir}/${file}`).default
+            commands.push(command)
+            client.commands.set(command.name, command)
+        });
+    }
 
     
 
